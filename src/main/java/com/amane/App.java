3b73d13eeeca7d635a4e8aaa47f8035e67a73080
salted_fish <60974962+1031923248@users.noreply.github.com>
2021-09-01 14:40:51 +0800

    Update App.java

--- src/main/java/com/amane/App.java
+++ src/main/java/com/amane/App.java
@@ -9,6 +9,6 @@ public class App {
         return i+s;
     }
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );asdfsadfasdfasdfsadf
     }
 }
